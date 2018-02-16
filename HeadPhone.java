cae0454caddcba5901c21724d8609b54195114a7
Sovereignpawn <sovereignpawn@outlook.com>
2018-02-15 19:44:30 -0500

    first commit

---
+++ HeadPhone.java
@@ -0,0 +1,20 @@
+/* Three constants named LOW, MEDIUM and HIGH with values of 1, 2 and 3 to denote the
+ * headphone volume.
+ * A private int data field named volume that specifies the volume of the headphone. The
+ * default volume is MEDIUM.
+ * A private boolean data field named pluggedIn that specifies if the headphone is plugged
+ * in. The default value is false.
+ * A private String data field named manufacturer that specifies the name of the
+ * manufacturer of the headphones.
+ * A private Color data field named headPhoneColor that specifies the color of the
+ * headphones.
+ * A private String data field named headPhoneModel that specifies the Model of the
+ * headphones.
+ * getter and setter methods for all data fields.
+ * A no argument constructor that creates a default headphone.
+ * A method named toString() that returns a string describing the current field values of
+ * the headphones.
+ * A method named changeVolume(value) that changes the volume of the headphone to
+ * the value passed into the method
+ */
+
